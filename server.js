e94719b44d9b564c26b5e9ada56da91077689272
NAMBIRAJA M <nambirajam.ug.21.cs@scadengineering.ac.in>
2024-08-15 20:52:10 +0530

    added functionality for cart icon

--- server.js
+++ server.js
@@ -19,56 +19,6 @@ db.connect();
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: true }));
 
-/* const recipeJSON = [
-    {
-        "id": "1",
-        "type": "protein bowl",
-        "name": "THE PERUVIAN RICE BOWL",
-        "price": 219,
-        "image": "Assets/peruvianbowl.jpg",
-        "veg": "Assets/veg_icon (1).png",
-        "nonveg": "Assets/non-veg_icon (1).png",
-        "ingredients": ["Mint", "Coriander", "Jalapeno", "Mayo", "Olive Oil", "Salt", "Ginger", "Red Chilli", "Rice"],
-        "nutrients": {
-            "kcal": 248,
-            "protein": "23g",
-            "Carbs": "32g",
-            "Fat": "34g",
-            "Fiber": "24g"
-        }
-    },
-    {
-        "id": "2",
-        "type": "protein bowl",
-        "name": "THE KERALA BOWL",
-        "price": 229,
-        "image": "Assets/The_Kerala_Bowl.jpg",
-        "veg": "Assets/veg_icon (1).png",
-        "nonveg": "Assets/non-veg_icon (1).png",
-        "ingredients": ["Herb Brown Rice", "Sauteed Veggies", "Zoodle Salad", "Pickled Onion", "Tomato Relish", "Coconut Milk"],
-        "nutrients": {
-            "kcal": 673,
-            "protein": "27g",
-            "Carbs": "80g",
-            "Fat": "25g",
-            "Fiber": "6.5g"
-        }
-    }
-]; */
-
-/* app.get("/datas",async (req,res) => {
-    const result= await db.query("SELECT * FROM cartinfo");
-   result.rows.forEach((recidata)=>{
-      console.log(recidata.name);
-   })
-  console.log(typeof result.rows);
-     const recipesdata=result.rows;
-     const jsondata=JSON.stringify(recipesdata);
-     console.log(jsondata.name);
-     console.log(typeof jsondata);
-
-
-}) */
 
 
 
@@ -105,6 +55,8 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
     res.render("contact.ejs");
 });
+
+
 app.post("/cart", async (req, res) => {
     const items = req.body.recipes;
     let itemid = [];
@@ -116,10 +68,14 @@ app.post("/cart", async (req, res) => {
    const cartItem = await checkcart(itemid);
     console.log(typeof cartItem);
     console.log("cartitems",cartItem);
-   /*  res.render("cart.ejs", { orders: cartItem }); */
+    res.render("cart.ejs", { orders: cartItem });
 });
-app.get("/cart", (req, res) => {
-    res.render("cart.ejs");
+
+
+app.get("/cart", async(req, res) => {
+    const result = await db.query("SELECT * FROM cartinfo");
+    const cartItem = result.rows;
+    res.render("cart.ejs",{ orders: cartItem });
 });
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
