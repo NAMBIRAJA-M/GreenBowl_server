4e48475802ed0c1c2b73d902cc419bd9f3f52788
NAMBIRAJA M <nambirajam.ug.21.cs@scadengineering.ac.in>
2024-08-16 21:53:58 +0530

    added the database and cart funtionalities

---
+++ database.sql
@@ -0,0 +1,57 @@
+/* //CREATING TABLE CARTINFO  */
+CREATE TABLE cartinfo (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(100) NOT NULL,
+    name VARCHAR(200) NOT NULL UNIQUE,
+    price INTEGER NOT NULL,
+    image VARCHAR(200) NOT NULL,
+    veg VARCHAR(100) NOT NULL,
+    nonveg VARCHAR(100) NOT NULL,
+    ingredients JSONB,
+    kcal INTEGER,
+    protein VARCHAR(10),
+    carbs VARCHAR(10),
+    fat VARCHAR(10),
+    fiber VARCHAR(10)
+);
+
+/* INSERTING DATA INTO cartinfo */
+INSERT INTO cartinfo (type, name, price, image, veg, nonveg, ingredients, kcal, protein, carbs, fat, fiber)
+VALUES (
+    'Protein Bowl',
+    'THE PERUVIAN RICE BOWL',
+    219,
+    'Assets/peruvianbowl.jpg',
+    'Assets/veg_icon (1).png',
+    'Assets/non-veg_icon (1).png',
+    '["Mint", "Coriander", "Jalapeno", "Mayo", "Olive Oil", "Salt", "Ginger", "Red Chilli", "Rice"]',
+    248,
+    '23g',
+    '32g',
+    '34g',
+    '24g'
+);
+
+/* deleting the records */
+TRUNCATE TABLE cartinfo RESTART IDENTITY;
+ALTER SEQUENCE cartinfo_id_seq RESTART WITH 1;
+
+/* display the records */
+SELECT * FROM cartinfo;
+
+
+/* //CREATING TABLE CARTINFO  */
+CREATE TABLE cartitems (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(100) NOT NULL,
+    name VARCHAR(200) NOT NULL UNIQUE,
+    price INTEGER NOT NULL,
+    image VARCHAR(200) NOT NULL,
+);
+
+/* deleting the records */
+TRUNCATE TABLE cartitems RESTART IDENTITY;
+ALTER SEQUENCE cartitems_id_seq RESTART WITH 1;
+
+/* display the records */
+SELECT * FROM cartitems;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyparser from "body-parser";
 import pg from "pg";
-
+import fs from 'fs';
 
 
 const app = express();
@@ -20,33 +20,78 @@ app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: true }));
 
 
+const jsonData = JSON.parse(fs.readFileSync('recipe.json', 'utf8'));
 
 
-async function checkcart(id) {
-    let cartdata = [];
-    const value = id[0]; 
-    const result = await db.query("SELECT * FROM cartinfo WHERE id=($1)", [value]);
-    result.rows.forEach((res) => {
-        cartdata.push(res);
-    });
-    return cartdata;
+async function checkcart(ids) {
+    try {
+        for (const id of ids) {
+            console.log(`Processing item with id: ${id}`);
+
+            const result = await db.query("SELECT * FROM cartinfo WHERE id = $1", [id]);
+            ids.length = 0;
+            console.log(ids);
+            if (result.rows.length > 0) {
+                const { id, type, name, price, image } = result.rows[0];
+                await db.query(
+                    "INSERT INTO cartitems (id, type, name, price, image) VALUES ($1, $2, $3, $4, $5)",
+                    [id, type, name, price, image]
+                );
+
+            }
+
+        }
+        return 'Cart check completed successfully.';
+    } catch (err) {
+        console.error('Error processing cart:', err);
+        return err.code;
+    }
 }
 
 
 
+const insertData = async () => {
+
+    for (const item of jsonData) {
+
+        const { type, name, price, image, veg, nonveg, ingredients, kcal, protein, carbs, fat, fiber } = item;
+
+        const formattedIngredients = Array.isArray(ingredients) ? JSON.stringify(ingredients) : ingredients;
+        /* console.log( formattedIngredients ) */
+        try {
+
+            await db.query("INSERT INTO cartinfo (type, name, price, image, veg, nonveg, ingredients, kcal, protein, carbs, fat, fiber)VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12) ON CONFLICT (name) DO NOTHING;", [type, name, price, image, veg, nonveg, formattedIngredients, kcal, protein, carbs, fat, fiber]);
+            console.log('Data inserted');
+
+        } catch (err) {
+            console.error('Error inserting data:', err);
+
+
+        }
+    }
+
+
+};
+insertData();
 
 app.get("/", (req, res) => {
+
+
     res.render("index.ejs");
 
 });
 
 
 app.get("/menu", async (req, res) => {
-    const result = await db.query("SELECT * FROM cartinfo");
-    const recipeJSON = result.rows;
-    res.render("menupage.ejs", { recipes: recipeJSON });
-    /*   console.log(Array.isArray(recipeJSON)); */
-});
+   /*  let message = {};
+    if (req.query.message) {
+        message = JSON.parse(decodeURIComponent(req.query.message));
+    } */
+        const result = await db.query("SELECT * FROM cartinfo");
+        const recipeJSON = result.rows;
+        res.render("menupage.ejs", { recipes: recipeJSON/* ,message:message  */});
+        /*   console.log(Array.isArray(recipeJSON)); */
+    });
 
 app.get("/about", (req, res) => {
     res.render("about.ejs");
@@ -57,25 +102,50 @@ app.get("/contact", (req, res) => {
 });
 
 
+
+let itemid = [];
 app.post("/cart", async (req, res) => {
     const items = req.body.recipes;
-    let itemid = [];
+
     items.forEach((item) => {
         var data = JSON.parse(item);
         const id = data.id;
         itemid.push(id);
     });
-   const cartItem = await checkcart(itemid);
-    console.log(typeof cartItem);
-    console.log("cartitems",cartItem);
-    res.render("cart.ejs", { orders: cartItem });
+
+    const response = await checkcart(itemid);
+
+    console.log("response from function", response);
+    if (response === 'Cart check completed successfully.') {
+        const result = await db.query("SELECT * FROM cartitems");
+        const cartItem = result.rows;
+        /*console.log(typeof cartItem);
+          console.log("cartitems", cartItem); */
+        res.render("cart.ejs", { orders: cartItem });
+    }
+    else {
+            
+        res.redirect("/menu");
+       /*  const message = { news: "already added" };
+        res.redirect(`/menu?message=${encodeURIComponent(JSON.stringify(message))}`);
+
+ */
+    }
+
 });
 
 
-app.get("/cart", async(req, res) => {
-    const result = await db.query("SELECT * FROM cartinfo");
+app.get("/cart", async (req, res) => {
+    const result = await db.query("SELECT * FROM cartitems");
     const cartItem = result.rows;
-    res.render("cart.ejs",{ orders: cartItem });
+    res.render("cart.ejs", { orders: cartItem });
+});
+
+app.get("/cart/delete/:id",async(req,res)=>{
+    const id = parseInt(req.params.id);
+    console.log("delete id ",id);
+    const result= await db.query("DELETE FROM cartitems WHERE id=$1",[id]);
+    res.redirect("/cart");
 });
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
