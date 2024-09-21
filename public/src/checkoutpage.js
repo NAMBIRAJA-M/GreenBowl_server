45e7f275d1bf08b1e3142af254920910abd448ec
NAMBIRAJA M <nambirajam.ug.21.cs@scadengineering.ac.in>
2024-09-21 23:01:10 +0530

    improve the UI elements in delivery service page

--- public/src/checkoutpage.js
+++ public/src/checkoutpage.js
@@ -1,17 +1,46 @@
-$(".deliverymode").click(function () {
-    const isdisabled = $('.address').prop("disabled")
 
-    if (isdisabled) {
-        $(".address").prop("disabled", false);
-    } else {
-        $(".address").prop("disabled", true);
-    }
+$(".deliverymode").click(function () {
+  $(".btn-addr").toggleClass("extracss");
+  
+
+  const isdisabled = $('.btn-addr').prop("disabled")
+
+  if (isdisabled) {
+      $(".btn-addr").prop("disabled", false);
+  } else {
+      $(".btn-addr").prop("disabled", true);
+      $(".address-container").removeClass("open")
+  }
 
 });
 
 
 function addressDetails(){
-  $(".address1").click(function(){
-    $(".address-container").addClass("open")
+  $(".btn-addr").click(function(){
+    $(".address-container").toggleClass("open")
   })
 }
+
+
+/* INPUT - LABEL FORM */
+const inputlabelid= $
+
+const label=$("#name");
+$(".forms-container input,.forms-container textarea").focus(function (event){
+  const id=event.target.id;
+  console.log(id);
+  $("#"+id).css({
+    "top": "-0.99rem",
+    "background-color": "white",
+    "font-size": "1.2rem"
+});
+});
+$(".forms-container input,.forms-container textarea").blur(function (event){
+  const id=event.target.id;
+  console.log(id);
+  $("#"+id).css({
+    "top": "0.8rem",
+    "background-color": "transparent",
+    "font-size": " "
+});
+})
