0c4eba4885ebf2ece1a780b140c067bb27f36737
NAMBIRAJA M <nambirajam.ug.21.cs@scadengineering.ac.in>
2024-09-22 21:48:55 +0530

    checkout page added functionality

--- public/src/checkoutpage.js
+++ public/src/checkoutpage.js
@@ -1,46 +1,123 @@
 
+
 $(".deliverymode").click(function () {
   $(".btn-addr").toggleClass("extracss");
-  
+
 
   const isdisabled = $('.btn-addr').prop("disabled")
 
   if (isdisabled) {
-      $(".btn-addr").prop("disabled", false);
+    $(".btn-addr").prop("disabled", false);
   } else {
-      $(".btn-addr").prop("disabled", true);
-      $(".address-container").removeClass("open")
+    $(".btn-addr").prop("disabled", true);
+    $(".address-container").removeClass("open")
+  }
+
+});
+
+function logincredentials() {
+  const isdisabled = $('.login-container').css("display");
+  if (isdisabled === "none") {
+    $(".login-container").css("display", "block");
+    $(".btn-addr1").css({
+      "background-color": " #88D66C",
+      "color": "white",
+    });
+    $(".login-credentials .icon1").css("color", "white")
+  } else {
+    $(".login-container").css("display","none");
+    $(".btn-addr1").css({
+      "background-color": "",
+      "color": "",
+    });
+    $(".login-credentials .icon1").css("color", "#399918")
+  }
+}
+function ordersSummary() {
+  const isdisabled = $('.orders-section').css("display");
+  if (isdisabled === "none") {
+    $(".orders-section").css("display", "block");
+    $(".btn-addr2").css({
+      "background-color": " #88D66C",
+      "color": "white",
+    });
+    $(".summary .icon1").css("color", "white")
+  }
+  else {
+    $(".orders-section").css("display", "none");
+    $(".btn-addr2").css({
+      "background-color": "",
+      "color": "",
+    });
+    $(".summary .icon1").css("color", "#399918")
+  }
+}
+function addressDetails() {
+  const isdisabled = $('.address-container').css("display");
+  if (isdisabled === "none") {
+    $(".address-container").css("display", "flex");
+    $(".btn-addr").css({
+      "background-color": " #88D66C",
+      "color": "white",
+    });
+    $(".address1 .icon1").css("color", "white")
+  }
+  else {
+    $(".address-container").css("display", "none");
+    $(".btn-addr").css({
+      "background-color": "",
+      "color": "",
+    });
+    $(".address1 .icon1").css("color", "#399918")
+  }
+}
+
+
+/* INPUT - LABEL FORM */
+const inputlabelid = $
+
+const label = $("#name");
+$(".forms-container input,.forms-container textarea").focus(function (event) {
+  const id = event.target.id;
+  console.log(id);
+  $("#" + id).css({
+    "top": "-0.99rem",
+    "background-color": "white",
+    "font-size": "1.2rem"
+  });
+});
+/*  */
+
+$(".forms-container input,.forms-container textarea").change(function (event) {
+  const inputvalue = event.target.value;
+  const id = event.target.id;
+  console.log(id);
+  console.log(inputvalue)
+
+  if (!inputvalue) {
+    $("#" + id).css({
+      "top": "0.8rem",
+      "background-color": "transparent",
+      "font-size": " "
+    });
+  }
+  else {
+    $("#" + id).css({
+      "top": "-0.99rem",
+      "background-color": "white",
+      "font-size": "1.2rem"
+    });
+  }
+  if (!inputvalue) {
+    console.log("excecuting")
+    $("#" + id).css({
+      "top": "0.8rem",
+      "background-color": "transparent",
+      "font-size": " "
+    });
   }
 
 });
 
 
-function addressDetails(){
-  $(".btn-addr").click(function(){
-    $(".address-container").toggleClass("open")
-  })
-}
 
-
-/* INPUT - LABEL FORM */
-const inputlabelid= $
-
-const label=$("#name");
-$(".forms-container input,.forms-container textarea").focus(function (event){
-  const id=event.target.id;
-  console.log(id);
-  $("#"+id).css({
-    "top": "-0.99rem",
-    "background-color": "white",
-    "font-size": "1.2rem"
-});
-});
-$(".forms-container input,.forms-container textarea").blur(function (event){
-  const id=event.target.id;
-  console.log(id);
-  $("#"+id).css({
-    "top": "0.8rem",
-    "background-color": "transparent",
-    "font-size": " "
-});
-})
