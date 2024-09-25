9b228df153e42c6a8c2df16a0c287f80e7f37360
NAMBIRAJA M <nambirajam.ug.21.cs@scadengineering.ac.in>
2024-09-25 19:31:58 +0530

    improved ui /ux in delivery service web page

--- public/src/checkoutpage.js
+++ public/src/checkoutpage.js
@@ -1,15 +1,48 @@
-
 const storedItems = JSON.parse(localStorage.getItem('cartItems'));
 
+if (storedItems && storedItems.length > 0) {
+  storedItems.forEach(item => {
+    const itemElement = $(`
+      <div class="order-item">
+        <img src="${item.image}" alt="${item.name}" />
+        
+      <div class="order-details">
+        <p class="order-name">${item.item_name}</p>
+        <p class="order-type">(${item.type})</p>
+        <p class="order-price">Price: ${item.price}</p>
+        </div>
+      </div>
+    `);
+    
+
+    $(".orders-section").append(itemElement);
+
+    $(".input-name").attr("value",`${item.name}`)
 
 
-if (storedItems) {
-  storedItems.forEach(element => {
-    $(".orders-section img").attr("src", element.image);
   });
 } else {
-  console.log('No items found in localStorage');
+  const itemElement=$(`
+    <p>'No items found in localStorage'</p>
+    `);
+    $(".orders-section").append(itemElement);
 }
+
+
+/* PRICE DETAILS */
+const packageingPrice = 30;
+const deliveryCharge = 40;
+
+const storedTotal = Number(localStorage.getItem('FinalTotal'));
+console.log(storedTotal);
+const storedOrders = Number(localStorage.getItem('orders'))
+document.querySelector(".Total-price").textContent = `Rs.${storedTotal}`;
+document.querySelector(".price-orders").textContent = `Price(${storedOrders} item)`;
+
+let totalPayable;
+totalPayable = storedTotal + deliveryCharge + packageingPrice;
+document.querySelector(".total-payable").textContent = `Rs.${totalPayable}`;
+
 
 $(".deliverymode").click(function () {
   $(".btn-addr").toggleClass("extracss");
@@ -18,10 +51,18 @@ $(".deliverymode").click(function () {
   if (isdisabled) {
     $(".btn-addr").prop("disabled", false);
     $(".delivery-charge,.delivery-charge-price").css("text-decoration", "");
+    totalPayable = storedTotal + deliveryCharge + packageingPrice;
+    document.querySelector(".total-payable").textContent = `Rs.${totalPayable}`;
   } else {
     $(".btn-addr").prop("disabled", true);
     $(".address-container").removeClass("open")
-    $(".delivery-charge,.delivery-charge-price").css("text-decoration", "line-through");
+    $(".delivery-charge,.delivery-charge-price").css({
+      "text-decoration":"line-through",
+      "text-decoration-color":"#399918",
+      "text-decoration-thickness":"0.189rem"
+    });
+    totalPayable = storedTotal + packageingPrice;
+    document.querySelector(".total-payable").textContent = `Rs.${totalPayable}`;
 
 
   }
@@ -29,16 +70,16 @@ $(".deliverymode").click(function () {
 });
 
 function logincredentials() {
-  const isdisabled = $('.login-container').css("display");
+  const isdisabled = $('.login-details').css("display");
   if (isdisabled === "none") {
-    $(".login-container").css("display", "block");
+    $(".login-details").css("display", "flex");
     $(".btn-addr1").css({
       "background-color": " #88D66C",
       "color": "white",
     });
     $(".login-credentials .icon1").css("color", "white")
   } else {
-    $(".login-container").css("display", "none");
+    $(".login-details").css("display", "none");
     $(".btn-addr1").css({
       "background-color": "",
       "color": "",
@@ -49,7 +90,7 @@ function logincredentials() {
 function ordersSummary() {
   const isdisabled = $('.orders-section').css("display");
   if (isdisabled === "none") {
-    $(".orders-section").css("display", "block");
+    $(".orders-section").css("display", "flex");
     $(".btn-addr2").css({
       "background-color": " #88D66C",
       "color": "white",
@@ -107,6 +148,8 @@ $(".forms-container input,.forms-container textarea").change(function (event) {
   console.log(id);
   console.log(inputvalue)
 
+  const value=$(".forms-container input").attr("value");
+
   if (!inputvalue) {
     $("#" + id).css({
       "top": "0.8rem",
@@ -133,24 +176,31 @@ $(".forms-container input,.forms-container textarea").change(function (event) {
 });
 
 
-
-/* PRICE DETAILS */
-const packageingPrice = 30;
-const deliveryCharge = 40;
-
-const storedTotal = Number(localStorage.getItem('FinalTotal'));
-console.log(storedTotal);
-const storedOrders = Number(localStorage.getItem('orders'))
-document.querySelector(".Total-price").textContent = `Rs.${storedTotal}`;
-document.querySelector(".price-orders").textContent = `Price(${storedOrders} item)`;
-
-let totalPayable;
-const isdisabled = $('.btn-addr').prop("disabled");
+/* const isdisabled = $('.btn-addr').prop("disabled");
 if (isdisabled) {
   totalPayable = storedTotal + packageingPrice;
 } else {
-  totalPayable = storedTotal + deliveryCharge + packageingPrice;
-}
+ 
+} */
 
-document.querySelector(".total-payable").textContent = `Rs.${totalPayable}`;
+document.addEventListener('DOMContentLoaded', function () {
+const valueName=$(".input-name").attr("value");
+const valueMobile=$(".input-name").attr("value");
+if(valueName){
+  $("#name").css({
+  "top": "-0.99rem",
+  "background-color": "white",
+  "font-size": "1.2rem"
+  });
+}else{
+  $("#name").css({
+    "top": "0.8rem",
+    "background-color": "transparent",
+    "font-size": " "
+  });
+}
+if(valueName && valueMobile){
+  
+}
+});
 
